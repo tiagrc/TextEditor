b48fa322695862a1484aedd456493e1aaf08fc92
tiago <tiago.rodrigues.tr99@hotmail.com>
2024-09-09 22:39:00 -0300

    começando novo projeto

---
+++ TextEditor/Program.cs
@@ -0,0 +1,39 @@
+﻿
+namespace TextEditor
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("**** SEJA BEM-VINDO (A) ****");
+            Console.WriteLine("1 - Criar novo arquivo");
+            Console.WriteLine("2 - Abrir arquivo");
+            Console.WriteLine("0 - Sair");
+            Console.Write("O que você deseja fazer? ");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: Edit(); break;
+                case 2: Open(); break;
+                default: Menu(); break;
+            }
+            static void Open()
+            {
+
+            }
+            static void Edit()
+            {
+
+            }
+        }
+
+    }
+}
